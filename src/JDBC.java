134c915e06de8647df2139da05accb3bb863d6d9
Vihanga Sachintha <vihanga1sachintha@gmail.com>
2026-08-09 19:44:58 +0530

    Create a database connection and make a read operation

--- src/JDBC.java
+++ src/JDBC.java
@@ -1,5 +1,33 @@
+import java.sql.*;
+
 public class JDBC {
     public static void main(String[] args) {
+        String url = "jdbc:mysql://localhost:3306/jdbc";
+        String user = "root";
+        String password = "Viha@sachi2002";
+        String query = "SELECT * from users";
 
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            Connection connection = DriverManager.getConnection(url, user, password);
+
+            Statement statement = connection.createStatement();
+            statement.executeQuery(query);
+
+            ResultSet resultSet = statement.executeQuery(query);
+
+            String name;
+            int age;
+            while (resultSet.next()) {
+                name = resultSet.getString(1);
+                age = resultSet.getInt(2);
+                System.out.println(name + " " + age);
+            }
+
+            connection.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
     }
 }
